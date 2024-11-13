bde9de720be13f45bbb10d5768545177d709f4cb
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-11-13 10:05:20 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce/pages/pages.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignInPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/pages/pages.dart
@@ -0,0 +1,5 @@
+import 'package:ecommerce/shared/shared.dart';
+import 'package:flutter/material.dart';
+
+part 'ui/home_page.dart';
+part 'ui/sign_in_page.dart';
